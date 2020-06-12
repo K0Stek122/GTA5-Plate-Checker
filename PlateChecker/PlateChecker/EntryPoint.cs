0d7d8f5f69ed275029da02a1f1e5626d9ea8ce16
K0Stek122 <48300772+K0Stek122@users.noreply.github.com>
2020-06-12 17:06:54 +0100

    Add files via upload

---
+++ PlateChecker/PlateChecker/EntryPoint.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Rage;
+
+[assembly: Rage.Attributes.Plugin("PlateChecker", Description = "Lets you check Plates in front of you", Author = "K0Stek")]
+namespace PlateChecker
+{
+    public static class EntryPoint
+    {
+        private static Vehicle GetVehicleInFront(float Distance)
+        {
+            Vector3 offSetPos = Game.LocalPlayer.Character.CurrentVehicle.GetOffsetPosition(Vector3.RelativeFront * Distance);
+            Vehicle[] vehicleList = Game.LocalPlayer.Character.GetNearbyVehicles(16);
+            foreach (Vehicle veh in vehicleList)
+            {
+                if (!veh.HasSiren && veh != Game.LocalPlayer.Character.CurrentVehicle)
+                {
+                    if (Vector3.Distance(offSetPos, veh.Position) < Distance)
+                        return veh;
+                }
+            }
+            return null;
+        }
+
+        public static void Main()
+        {
+            InitializationFile iniFile = new InitializationFile("plugins//PlateChecker.ini");
+
+            //Keys key = iniFile.Read<Keys>("Keys", "ReadPlate", Keys.F3);
+            Keys key = iniFile.ReadEnum<Keys>("Keys", "ReadPlate", Keys.F3);
+
+            GameFiber.StartNew(delegate
+            {
+                while (true)
+                {
+                    GameFiber.Yield();
+                    if (Game.IsKeyDown(key))
+                    {
+                        Vehicle closestVehicle = Game.LocalPlayer.Character.IsInAnyVehicle(false) ? GetVehicleInFront(7.5f) : GetVehicleInFront(4.2f);
+                        if (closestVehicle != null)
+                            Game.DisplayHelp("Plate: " + closestVehicle.LicensePlate.ToString());
+                    }
+                }
+            });
+        }
+    }
+}
